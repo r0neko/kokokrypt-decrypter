50708a65c4e59dd0caabffbd3acc1ada430ee63f
Talnaci Alexandru <talnacialexandru@yahoo.com>
2020-04-15 02:10:21 +0300

    Added source code

---
+++ KoKoKrypt Decrypter/dc.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class dc
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.file = New System.Windows.Forms.Label()
+        Me.fname = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'file
+        '
+        Me.file.AutoSize = True
+        Me.file.Location = New System.Drawing.Point(13, 13)
+        Me.file.Name = "file"
+        Me.file.Size = New System.Drawing.Size(78, 13)
+        Me.file.TabIndex = 0
+        Me.file.Text = "decrypting file: "
+        '
+        'fname
+        '
+        Me.fname.AutoSize = True
+        Me.fname.Location = New System.Drawing.Point(12, 36)
+        Me.fname.Name = "fname"
+        Me.fname.Size = New System.Drawing.Size(23, 13)
+        Me.fname.TabIndex = 1
+        Me.fname.Text = "null"
+        '
+        'dc
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(308, 94)
+        Me.Controls.Add(Me.fname)
+        Me.Controls.Add(Me.file)
+        Me.Name = "dc"
+        Me.Text = "dc"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents file As System.Windows.Forms.Label
+    Friend WithEvents fname As System.Windows.Forms.Label
+End Class
---
+++ KoKoKrypt Decrypter/dc.vb
@@ -0,0 +1,41 @@
+﻿Public Class dc
+
+    Private Sub dc_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Decrypt()
+    End Sub
+    Private Sub Decrypt()
+        Dim mExtensions As IEnumerable(Of String) = {".kokolocker"}
+        Dim route As String = "C:\Batch"
+        For Each mArchives As String In searchArchives(route, mExtensions)
+            fname.Text = mArchives.ToString
+            Try
+                CryptoRansomwareDecyrption(mArchives, "KoKoKrypterV1.0")
+            Catch ex As Exception
+                Continue For
+            End Try
+        Next
+    End Sub
+    Private Function AESD(ByVal input As Byte(), ByVal key As String) As Byte()
+        Dim AES As New System.Security.Cryptography.RijndaelManaged
+        Dim SHA256 As New System.Security.Cryptography.SHA256Cng
+        Try
+            AES.Key = SHA256.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(key))
+            AES.Mode = Security.Cryptography.CipherMode.ECB
+            Dim DESDecrypter As System.Security.Cryptography.ICryptoTransform = AES.CreateDecryptor
+            Dim Buffer As Byte() = input
+            Return DESDecrypter.TransformFinalBlock(Buffer, 0, Buffer.Length)
+        Catch ex As Exception
+        End Try
+    End Function
+    Public Function CryptoRansomwareDecyrption(ByVal file As String, ByVal key As String)
+        If My.Computer.FileSystem.FileExists(file) Then
+            Dim fil As String = System.IO.Path.GetFileName(file)
+            My.Computer.FileSystem.WriteAllBytes(file, AESD(My.Computer.FileSystem.ReadAllBytes(file), key), False)
+            fil = fil.Replace(".kokolocker", "").Trim()
+            My.Computer.FileSystem.RenameFile(file, fil)
+        End If
+    End Function
+    Public Shared Function searchArchives(ByVal route As String, ByVal mExtensions As IEnumerable(Of String)) As IEnumerable(Of String)
+        Return (From mArchives In IO.Directory.GetFiles(route, "*", IO.SearchOption.AllDirectories) Where mExtensions.Contains(IO.Path.GetExtension(mArchives).ToLower())).ToList()
+    End Function
+End Class
